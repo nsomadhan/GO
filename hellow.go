eb3166e6b02623b959274166b8443f9337bd9121
nsomadhan <nsomadhan@gmail.com>
2022-08-21 16:19:23 +0600

    go comment

--- hellow.go
+++ hellow.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+
+// This is single line comment
+
+/* This is muline 
+ 	comment */
+
 func main() {
 
 	fmt.Println("Hellow this is first Go program");
@@ -10,7 +16,7 @@ func main() {
 
 	int_variabe := "12";
 	fmt.Println(int_variabe);
-	fmt.Printf("T", int_variabe) 
+	fmt.Printf("T", int_variabe) // Checking type of variable. 
 
 
 };
