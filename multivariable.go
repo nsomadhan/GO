0d600981a4816b9429f70b45380c4ee28bc7a0b8
nsomadhan <nsomadhan@gmail.com>
2022-08-21 17:12:02 +0600

    go multiline variable declaration

---
+++ multivariable.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+
+func main() {
+  
+ fmt.Println ("Go variable name are case sensetive");
+
+ var a, b, c int = 1, 2, 3;
+ fmt.Printf("T",a);
+ fmt.Println(b);
+ fmt.Println(c);
+
+
+
+ // Another way 
+
+ var x, y, z = 6, "string", 1.5
+ m, n := 7, "String"
+
+ fmt.Println(x)
+ fmt.Printf("T", x)
+ fmt.Println(y)
+ fmt.Println(z)
+ fmt.Printf("T", z)
+ fmt.Println(m)
+ fmt.Printf("T", m)
+ fmt.Println(n)
+
+
+}; 
