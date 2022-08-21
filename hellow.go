8e10e2db21f8d001986c1d188410f25fae6cd396
nsomadhan <nsomadhan@gmail.com>
2022-08-21 16:13:47 +0600

    first go hello

---
+++ hellow.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hellow this is first Go program");
+	fmt.Println("Bismillah");
+
+
+	int_variabe := "12";
+	fmt.Println(int_variabe);
+	fmt.Printf("T", int_variabe) 
+
+
+};
